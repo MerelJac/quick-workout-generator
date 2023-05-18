9e34d8a2b01d30d0d93384d75fb8804361272f9d
Merel Jacobs <merel.burleigh@gmail.com>
2023-05-18 11:16:23 -0700

    added CSS styling

--- assets/script.js
+++ assets/script.js
@@ -7,17 +7,25 @@ startWorkoutBtn.addEventListener("click", function() {console.log("Hi!")});
 
 var exercises = ['Push-Ups','Pull-Ups','Sit Ups','Plank','Squats','Deadlifts','Rear Foot Elevated Split Squats','Calf Raises','Jog','Chest Press','Bicep Curls','Single Leg Deadlifts','Fire Hydrants','Donkey Kicks','Tricep Extensions','Hip Bridges','Hip Thrusts','Pistol Squats']; 
 
+//playing with object sets - commented out for now
+// var exerciseSets =
+//     {move: 'Push Ups',
+//     reps: 'max',
+//     sets: '3',
+//     };
+// document.querySelector('section').innerHTML = exerciseSets.move;
+
 var workoutMaxLength = 10;
 
-// insert function printExercise
-
+// function printExercise
 var printExercises = function() {
     var exerciseList = [];
     for (var i = 0; i < workoutMaxLength; i++) {
     var pickRandom = exercises[Math.floor(Math.random() * exercises.length)];
     console.log(pickRandom);
     exerciseList.push(pickRandom);
-    document.querySelector('section').innerHTML = exerciseList;
+    exerciseList.join('\r\n'); // should this start a new line?
+    document.querySelector('#printed-exercises-section').innerHTML = exerciseList;
     }
     // next: separate variables AND prevent duplicates. 
 }   
